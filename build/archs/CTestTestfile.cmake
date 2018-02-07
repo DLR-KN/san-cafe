f7982a136100354e9c50be1847186b12b4b03fff
Jan Kraemer <kraemer.jn@gmail.com>
2018-02-07 09:18:24 +0100

    + Added .gitignore

--- build/archs/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/jkraemer/Dev/san-cafe/archs
-# Build directory: /home/jkraemer/Dev/san-cafe/build/archs
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(archs "test_archs")
